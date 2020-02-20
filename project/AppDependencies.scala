edcaae9889de6cfad75b4148c07be0df11bb8f14
Brenna Samuel <42062758+brennaksamuel@users.noreply.github.com>
2020-02-20 16:04:41 +0000

    SAR-5246: Updated headers and dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,21 +5,21 @@ object AppDependencies {
 
   val compile = Seq(
 
-    "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.20.0-play-25",
-    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.13.0",
-    "uk.gov.hmrc"             %% "play-ui"                  % "7.40.0-play-25"
+    "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.23.0-play-25",
+    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "5.1.0",
+    "uk.gov.hmrc"             %% "play-ui"                  % "8.8.0-play-25"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "hmrctest"                    % "3.9.0-play-25"         % "test, it",
-    "org.scalatest"           %% "scalatest"                   % "3.0.4"                 % "test",
+    "org.scalatest"           %% "scalatest"                   % "3.0.8"                 % "test",
     "com.typesafe.play"       %% "play-test"                   % current                 % "test",
     "org.pegdown"             %  "pegdown"                     % "1.6.0"                 % "test, it",
-    "uk.gov.hmrc"             %% "service-integration-test"    % "0.2.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"          % "2.0.0"                 % "test, it",
+    "uk.gov.hmrc"             %% "service-integration-test"    % "0.9.0-play-25"         % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"          % "2.0.1"                 % "test, it",
     "org.scalamock"           %% "scalamock-scalatest-support" % "3.6.0"                 % "test",
-    "org.mockito"             %  "mockito-core"                % "2.13.0"                % "test",
-    "com.github.tomakehurst"  %  "wiremock"                    % "2.6.0"                 % "it"
+    "org.mockito"             %  "mockito-core"                % "3.2.4"                 % "test",
+    "com.github.tomakehurst"  %  "wiremock"                    % "2.26.0"                % "it"
   )
 
 }
